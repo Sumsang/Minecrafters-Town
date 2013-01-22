c1cdbf1e95a28ac542c08f452b345d6d3744918d
Nico Serafin <nicoserafin@gmx.de>
2013-01-22 16:43:41 +0100

    Added plugin.yml

--- MCTown/src/net/mctown/MCTown/MCTown.java
+++ MCTown/src/net/mctown/MCTown/MCTown.java
@@ -1,5 +1,19 @@
 package net.mctown.MCTown;
 
-public class MCTown {
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class MCTown extends JavaPlugin {
+
+	public void onEnable() {
+		PluginDescriptionFile desc = this.getDescription();
+		this.getLogger().info(
+				desc.getFullName() + " version " + desc.getVersion()
+						+ " has been loaded!");
+	}
+
+	public void onDisable() {
+		PluginDescriptionFile desc = this.getDescription();
+		this.getLogger().info(desc.getFullName() + " has been unloaded!");
+	}
 }
